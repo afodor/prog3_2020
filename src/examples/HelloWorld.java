d3af450638d316c4b8983dc49061d1abccce49c4
vmittal2 <vmittal@uncc.edu>
2020-09-09 13:37:31 -0400

    Varnika's first commit

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld
 	{
 		System.out.println("Hello world; it is 1:11 but on my local computer overriding the github change!");
 		System.out.println("Hello World, John.");
+		System.out.println("Hi.. I am Varnika!! Looking forward to learn great things from Dr. Fodor");
+
 	}
 }
 
