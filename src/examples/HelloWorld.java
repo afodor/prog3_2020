756af2cd705e7925ccf579ddd17883682f64841f
erintsigh <59835854+erintsigh@users.noreply.github.com>
2020-09-09 14:10:13 -0400

    Erin's commit

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -7,23 +7,22 @@ public class HelloWorld
 	{
 		System.out.println("Hello world; it is 1:11 but on my local computer overriding the github change!");
 		System.out.println("Hello world from Eclipse");
-<<<<<<< HEAD
+
 		System.out.println("Hello World, John.");
 		System.out.println("Hello world, this is Juan. What is Git?");
 
-=======
+
           System.out.println("Hello World, John.");
           
           System.out.println("Hello world; it is 1:37 but on my local computer overriding the github change!");
 		System.out.println("Hello world from VSCODE");
 		System.out.println("Hello World, Brandon.");
-<<<<<<< HEAD
+
 		System.out.println("Hello World! It's Ali!");
-=======
+
 		System.out.println("Hello world, this is Juan. What is git?");
 		System.out.println("Hello world, this is Sarah! :-)");
 		System.out.println("Hello! It's Ali!");
->>>>>>> refs/remotes/origin/master
->>>>>>> refs/remotes/origin/master
+		System.out.println("Erin was here! ");
 	}
 }
