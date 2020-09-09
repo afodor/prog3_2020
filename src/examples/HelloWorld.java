efdcc684318cee3d314a6cefc470d22fddfbcad7
skharrison <sharr199@uncc.edu>
2020-09-09 13:19:59 -0400

    Sarah adding line

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello world; it is 1:11 but on my local computer overriding the github change!");
+		System.out.println("Hello world; This is Sarah!! :-)");
 	}
 }
 
