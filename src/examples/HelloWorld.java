3c232f9ec5ae02ed04d0298b6fe420dec8802dd0
Juan Bolanos <juanbolanos@Juans-MBP.attlocal.net>
2020-09-09 14:47:48 -0400

    Juan Making more changes..."

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello world; it is 1:11 but on my local computer overriding the github change!");
+		System.out.println("Hello world, this is Juan. What is git?");
+ 		System.out.println("Hello world, this is Juan. Testing this again, because I want to fully understand git?");
 	}
 }
 
