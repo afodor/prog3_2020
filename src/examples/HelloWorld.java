12ba67205e6a200af4cdc1f4bb204c8f21360de3
Anthony Fodor <anthony.fodor@gmail.com>
2020-09-09 13:33:08 -0400

    from Eclipse

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello world; it is 1:11 but on my local computer overriding the github change!");
+		System.out.println("Hello world from Eclipse");
+		
 	}
 }
 
