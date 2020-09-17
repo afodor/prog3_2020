1a580e722527a2220f82493be6fba91469de0c0f
Anthony Fodor <anthony.fodor@gmail.com>
2020-09-16 20:41:22 -0400

    time example

---
+++ src/examples/TimeExamples.java
@@ -0,0 +1,17 @@
+package examples;
+
+
+public class TimeExamples
+{
+	public static void main(String[] args)
+	{
+		byte b= 0;
+		
+		for( int x=0; x <1000; x++)
+		{
+			b = (byte) (b+1);
+			
+			System.out.println(b);
+		}
+	}
+}	
